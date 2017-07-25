48de4867eef80831d53be8aa6160e85b375be6ab
RacharlaNeha <nracharla22@gmail.com>
2017-07-25 21:51:52 +0000

    created custom resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,46 +6,16 @@
 #
 include_recipe 'lcd_webserver::users'
 
-package 'httpd'
-package 'net-tools'
-
-service 'httpd' do
-        action [:enable,:start]
-end
-
-#package 'net-tools' do
- # action :install
-#end
-
-#package 'httpd' do 
- # action :install 
-#end 
-
-package platform_package_httpd
-
-service platform_service_httpd  do 
-  action [:enable, :start] 
+hello_httpd 'greet world' do
+  greeting "Hello"
+  action :create
 end
 
 #package node['app']['language'] do
  # action :install
 #end
 
-template '/var/www/html/index.html' do
-  mode '0644'
-  owner 'apache'
-  group 'apache'
-  variables(
-    greeting_scope: 'World',
-    greeting: node['greeting'],
-    fqdn: node['fqdn']
-  )
-end
 
 execute 'systemctl restart httpd' do
   only_if { index_exists? }
 end
-
-
-#include_recipe 'lcd_webserver::users'
-
